e10b05f1ec4d64619a789f6b1652421c8ab61fab
AnupamGaurav23 <139894476+AnupamGaurav23@users.noreply.github.com>
2024-07-10 21:40:39 +0530

    Create Nene's_game.cpp

---
+++ Nene's_game.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define endl "\n"
+#define ll long long
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int modi = 1e9 + 7, n, a, b, m;
+
+set<ll> g[200005];
+vector<ll> dist(200005, 0);
+map<ll, ll> mp;
+set<pair<ll, ll>> st;
+
+
+void dfs(ll vertex, ll par) {
+    for (auto child : g[vertex]) {
+        if (child == par) continue;
+        dist[child] = dist[vertex] + 1;
+        mp[child] = vertex;
+        if (g[child].size() == 1) st.insert({dist[child], child});
+        dfs(child, vertex);
+    }
+
+
+}
+
+
+
+
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+
+    freopen("input.txt", "r", stdin) ;
+    freopen("outp.txt", "w", stdout);
+#endif
+    ll tc;
+    cin >> tc;
+    while (tc--) {
+
+        ll k, q; cin >> k >> q;
+        ll arr[k];
+        for (ll i(0); i < k; i++) cin >> arr[i];
+        ll mini = arr[0] - 1;
+        for (ll i(0); i < q; i++) {
+            ll hue; cin >> hue;
+            cout << min(hue, mini) << " ";
+        }
+        cout << endl;
+    }
+
+}
